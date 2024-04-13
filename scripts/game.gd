25fdc404072c9ddb15655d0888a591d99a9a6aa8
Liniux <153652735+Lin1ux@users.noreply.github.com>
2024-04-13 11:34:15 +0200

    Building UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var resource_slot = preload("res://interface/resource_slot.tscn")
 var item_slot = preload("res://interface/item_slot.tscn")
+var building_slot = preload("res://interface/building_slot.tscn")
+var building_cost = preload("res://interface/building_cost.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,13 +13,23 @@ func _ready():
 		s.get_node("Sprite").texture = Global.resources[r]["sprite"]
 		s.get_node("Count").text = str(Global.current_resources[r])
 		s.get_node("Name").text = Global.resources[r]["name"]
-		$UI/Resources.add_child(s)
+		$UI/UI/Resources.add_child(s)
 		
 	for i in Global.current_items:
 		var s = item_slot.instantiate()
 		s.get_node("Sprite").texture = Global.items[i]["sprite"]
 		s.get_node("Count").text = str(Global.current_items[i])
-		$UI/Items.add_child(s)
+		$UI/UI/Items.add_child(s)
+		
+	for i in Global.buildings:
+		var s = building_slot.instantiate()
+		s.get_node("Building").texture = Global.buildings[i]["sprite"]
+		for j in range(Global.buildings[i]["resource_type"].size()):
+			var c = building_cost.instantiate()
+			c.get_node("Sprite").texture = Global.resources[Global.buildings[i]["resource_type"][j]]["sprite"]
+			c.get_node("Count").text = str(Global.buildings[i]["resource_cost"][j])
+			s.get_node("Cost").add_child(c)
+		$UI/UI/Building/Panel/ScrollContainer/GridContainer.add_child(s)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
