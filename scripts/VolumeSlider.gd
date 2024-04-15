878fbc87d8b09eb74373b431f10f4247a83385b2
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-04-15 21:00:34 +0200

    Volume fix

--- scripts/VolumeSlider.gd
+++ scripts/VolumeSlider.gd
@@ -1,5 +1,9 @@
 extends HSlider
 
+@onready var _bus := AudioServer.get_bus_index("Master")
+
+func _ready():
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
 
 func _on_drag_ended(value_changed):
-	Global.volume = value / max_value
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
