042459137fb77ba37f3950183e804f4ccd70251c
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-04-15 14:39:56 +0200

    Fixes, enemy spawn locations

--- scripts/game.gd
+++ scripts/game.gd
@@ -71,14 +71,6 @@ func _process(delta):
 
 
 var resource_point = preload("res://object/resource_point.tscn")
-
-func _on_resource_spawn_timer_timeout():
-	if get_tree().get_nodes_in_group("resource_point").size() < 10:
-		for i in range(3):
-			var rp = resource_point.instantiate()
-			rp.global_position = Vector2(randi_range(0, 640), randi_range(0,360))
-			rp.resource_type = Global.resources.keys().pick_random()
-			$GameSpace.add_child(rp)
 			
 func mousePos():
 	return get_global_mouse_position()
